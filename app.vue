a9d6ff07a778020a4419d04bb3d143dd1fc91309
Youness0f <146443299+Youness0f@users.noreply.github.com>
2023-09-29 14:05:49 +0100

    Create app.vue
    Yup yup

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Your Vue.js/Nuxt.js frontend code here -->
+    <!-- Include the HTML structure for your web application -->
+  </div>
+</template>
+
+<script>
+export default {
+  // Your Vue.js/Nuxt.js frontend JavaScript code here
+}
+</script>
+
+<style>
+/* Your CSS styles here */
+</style>
